d2f9df7acd678449cc3f00b128ea7ca4fa540f21
Lakshmi256 <lp268742@gmail.com>
2020-01-30 10:58:47 +0530

    [lakshmi prasad]add.added getusername and update password in userrepoimpl

--- Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/repository/USerRepositoryImplementation.java
+++ Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/repository/USerRepositoryImplementation.java
@@ -2,11 +2,11 @@ package com.bridgelabz.fundoonotes.repository;
 
 import javax.persistence.EntityManager;
 
-
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.bridgelabz.fundoonotes.entity.PasswordUpdate;
 import com.bridgelabz.fundoonotes.entity.UserInformation;
 
 public class USerRepositoryImplementation implements UserRepository{
@@ -25,6 +25,31 @@ public class USerRepositoryImplementation implements UserRepository{
 			Query q=session.createQuery("FROM UserInformation where email=:email");
 			q.setParameter("email",email);
 			return(UserInformation)q.uniqueResult();
+		}
+			@Override
+			public UserInformation getUserById(Long id) {
+				Session session= entityManager.unwrap(Session.class);
+				Query q=session.createQuery("FROM UserInformation where if=:id");
+				q.setParameter("id",id);
+				return(UserInformation)q.uniqueResult();
+						
+			}
+			@Override
+			public boolean upDate(PasswordUpdate information,Long id) {
+				Session session= entityManager.unwrap(Session.class);
+				Query q=session.createQuery("update UserInformation set password =:p"+" "+" "+"where id=:i");
+			q.setParameter("p",information.getConfirmPassword());
+			q.setParameter("i",id);
+			int status= q.executeUpdate();
+			if (status>0) {
+				return true;
+						
+			}
+			else {
+				return false;
+			}
+			
+			}
 	}
 
-}
+
--- Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
+++ Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.fundoonotes.repository;
 
+import com.bridgelabz.fundoonotes.entity.PasswordUpdate;
 import com.bridgelabz.fundoonotes.entity.UserInformation;
 
 public interface UserRepository {
 UserInformation save(UserInformation userInfromation);
 
 UserInformation getUser(String email);
+
+UserInformation getUserById(Long id);
+
+boolean upDate(PasswordUpdate information, Long id);
 }
