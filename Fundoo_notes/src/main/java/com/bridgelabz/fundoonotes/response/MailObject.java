d987a65f3da34dba21278a2090bad9ebfb8b16f0
Lakshmi256 <lp268742@gmail.com>
2020-02-03 11:20:17 +0530

    [lakshmi prasad]add.class mailservide provider

--- Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/response/MailObject.java
+++ Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/response/MailObject.java
@@ -9,8 +9,26 @@ import lombok.Data;
 @Component
 public class MailObject implements Serializable {
 	private static final long serialVesrionUID=1L;
-	String email;
-	String subject;
-	String message;
+	private String email;
+	private String subject;
+	private String message;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
 
 }
