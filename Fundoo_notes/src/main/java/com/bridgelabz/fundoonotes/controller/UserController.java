781e347040a541851d67be9fae7159139405d1a7
Lakshmi256 <lp268742@gmail.com>
2020-02-06 14:55:32 +0530

    [lakshmi prasad]add.getoneuser api in controller

--- Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
+++ Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.fundoonotes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -82,14 +85,27 @@ public class UserController {
 				.body(new Response("user does not exist with given email id", 400, email));
 
 	}
+
 	@PutMapping("user/update/{token}")
-	public ResponseEntity<Response> update(@PathVariable("token") String token,@RequestBody PasswordUpdate update)
-	{
-		boolean result=service.update(update, token);
-		if(result)
-		{
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("password updated successfully", 200, update));
+	public ResponseEntity<Response> update(@PathVariable("token") String token, @RequestBody PasswordUpdate update) {
+		boolean result = service.update(update, token);
+		if (result) {
+			return ResponseEntity.status(HttpStatus.ACCEPTED)
+					.body(new Response("password updated successfully", 200, update));
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Response("password  does not match", 402, update));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(new Response("password  does not match", 402, update));
+	}
+
+	@GetMapping("user/getusers")
+	public ResponseEntity<Response> getUsers() {
+		List<UserInformation> users = service.getUsers();
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("the users are", 200, users));
+	}
+
+	@GetMapping("user/getoneuser")
+	public ResponseEntity<Response> getOneUser(@RequestHeader("token") String token) {
+		UserInformation user = service.getsingleUser(token);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new Response("user is ", 200, user));
 	}
 }
