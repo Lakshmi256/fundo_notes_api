f48babf4f7d96a2164291e34c9839e1448ebaf98
Lakshmi256 <lp268742@gmail.com>
2020-02-07 09:20:11 +0530

    [lakshmi prasad]add.creating a label

---
+++ Fundoo_notes/src/main/java/com/bridgelabz/fundoonotes/implementation/LabelServiceImplementation.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.fundoonotes.implementation;
+
+import javax.transaction.Transactional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bridgelabz.fundoonotes.entity.LabelDto;
+import com.bridgelabz.fundoonotes.entity.UserInformation;
+import com.bridgelabz.fundoonotes.repository.UserRepository;
+import com.bridgelabz.fundoonotes.service.LabelService;
+import com.bridgelabz.fundoonotes.utility.JwtGenerator;
+
+@Service
+public class LabelServiceImplementation implements LabelService {
+
+private UserInformation user=new UserInformation();
+@Autowired
+private UserRepository userRepository;
+@Autowired
+private JwtGenerator tokenGenerator;
+@Autowired
+private ModelMapper modelMapper;
+@Transactional
+@Override
+public void createLabel(LabelDto label,String token) {
+	
+}
+}
